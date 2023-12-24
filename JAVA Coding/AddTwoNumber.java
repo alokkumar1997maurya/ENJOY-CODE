c6414e97b30808b7d771e61699204bbde2cb49b7
gsmalok <gsmalok@gmail.com>
2023-12-24 22:57:00 +0530

    done

--- JAVA Coding/AddTwoNumber.java
+++
@@ -1,5 +0,0 @@
-class AddTwoNumber{
-    public static void main(String[] args) {
-        System.out.println(10+10);
-    }
-}
--- JAVA Coding/SubtrackTwoNumber.java
+++
@@ -1,6 +0,0 @@
-public class SubtrackTwoNumber {
-    public static void main(String[] args) {
-        System.out.println(20-10);
-    }
-    
-}
